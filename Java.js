a85cf1958516b282476a1147aa9bcc752c1c6f23
AngelesGutierrez <angelesgutierrez573@gmail.com>
2018-07-13 15:35:50 -0700

    java

--- Java.js
+++ Java.js
@@ -1,4 +1,4 @@
-alert("k");
+alert("Welcome to my page");
 function myFunction() {
     document.getElementById("demo").innerHTML = "Im From San Leandro, CA and I go to San Leandro High School.";
 }
